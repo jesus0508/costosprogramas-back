6f3d3002f25ce9262444862271ddeeb119585db6
Jesus Alarcon <jesus.alarcon3@unmsm.edu.pe>
2019-09-19 21:45:31 -0500

    Estructura inicial del proyecto

---
+++ src/main/java/pe/edu/unmsm/sistemas/ModuloCostoPrograma.java
@@ -0,0 +1,13 @@
+package pe.edu.unmsm.sistemas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModuloCostoPrograma {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModuloCostoPrograma.class, args);
+	}
+
+}
