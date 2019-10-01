ec3fbb1a8ec23381a8f625267fd94f99a5fa9618
AULA F.DE ING.DE SISTEMAS E INF <AULAFISI@UNMSM.pe>
2019-10-01 16:12:37 -0500

    Arreglando Cors

--- src/main/java/pe/edu/unmsm/sistemas/configuration/CorsConfiguration.java
+++ src/main/java/pe/edu/unmsm/sistemas/configuration/CorsConfiguration.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class CorsConfiguration implements WebMvcConfigurer {
   @Override
   public void addCorsMappings(CorsRegistry registry) {
-    registry.addMapping("*").allowedMethods("GET", "PUT", "POST").allowedOrigins("*").allowedHeaders("*");
+    registry.addMapping("/**").allowedMethods("GET", "PUT", "POST").allowedOrigins("*").allowedHeaders("*");
   }
 
 }
