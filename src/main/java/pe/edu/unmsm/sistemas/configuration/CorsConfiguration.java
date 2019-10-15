b8595e5ad9bdb56177b6e42662a1412eb60b808c
Jesus Alarcon <jesus.alarcon3@unmsm.edu.pe>
2019-10-15 10:07:18 -0500

    Credenciales

--- src/main/java/pe/edu/unmsm/sistemas/configuration/CorsConfiguration.java
+++ src/main/java/pe/edu/unmsm/sistemas/configuration/CorsConfiguration.java
@@ -5,14 +5,15 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan
-public class CorsConfiguration implements WebMvcConfigurer {
+public class CorsConfiguration extends WebMvcConfigurerAdapter {
   @Override
   public void addCorsMappings(CorsRegistry registry) {
-    registry.addMapping("*").allowedMethods("GET", "POST").allowedOrigins("*").allowedHeaders("*");
+    registry.addMapping("*").allowedMethods("GET", "POST").allowedOrigins("*").allowedHeaders("*").allowCredentials(false);
   }
 
 }
